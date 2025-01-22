0b9c3b5eab7d7497d64fb68ba7529b21884f2292
涛叔 <hi@taoshu.in>
2025-01-22 17:28:40 +0800

    避免重复下载资源和重复构建

--- web/web.go
+++ web/web.go
@@ -1,4 +1,4 @@
 package web
 
-//go:generate curl -sLO https://unpkg.com/mithril/mithril.js
-//go:generate curl -sLO https://cdn.jsdelivr.net/npm/water.css@2/out/water.css
+//go:generate curl -sLO https://unpkg.com/mithril/mithril.js -z mithril.js
+//go:generate curl -sLO https://cdn.jsdelivr.net/npm/water.css@2/out/water.css -z water.css
