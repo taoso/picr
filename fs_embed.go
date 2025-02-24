acb1f7ff9ec31ce39f20c20f81b41cf6ab2aa7ea
涛叔 <hi@taoshu.in>
2025-02-24 11:18:42 +0800

    修复生产环境反复下载静态资源问题
    通过 go:embed 嵌入的静态文件不返回修改时间，导致每次打开页面时
    浏览器都会重新下载文件数据。

--- fs_embed.go
+++ fs_embed.go
@@ -3,18 +3,80 @@
 package main
 
 import (
+	"debug/buildinfo"
 	"embed"
 	"io/fs"
 	"net/http"
+	"os"
+	"time"
 )
 
 //go:embed web/*
 var webFS embed.FS
+
+var buildTime time.Time
+
+func init() {
+	exe, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	info, err := buildinfo.ReadFile(exe)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, s := range info.Settings {
+		if s.Key == "vcs.time" && s.Value != "" {
+			t, err := time.Parse(time.RFC3339, s.Value)
+			if err != nil {
+				panic(err)
+			}
+			buildTime = t
+			break
+		}
+	}
+}
+
+type embedFS struct {
+	fs.FS
+	modTime time.Time
+}
+
+func (f *embedFS) Open(name string) (fs.File, error) {
+	file, err := f.FS.Open(name)
+
+	return &embedFile{File: file, modTime: f.modTime}, err
+}
+
+type embedFile struct {
+	fs.File
+	modTime time.Time
+}
+
+func (f *embedFile) Stat() (os.FileInfo, error) {
+	fileInfo, err := f.File.Stat()
+
+	return &embedFileInfo{FileInfo: fileInfo, modTime: f.modTime}, err
+}
+
+type embedFileInfo struct {
+	os.FileInfo
+	modTime time.Time
+}
+
+func (f *embedFileInfo) ModTime() time.Time {
+	return f.modTime
+}
 
 func init() {
 	fsys, err := fs.Sub(webFS, "web")
 	if err != nil {
 		panic(err)
 	}
-	web = http.FS(fsys)
+	web = http.FS(&embedFS{
+		FS:      fsys,
+		modTime: buildTime,
+	})
 }
